0651bc82c327f88aeb2f2dc77afe071f3fe0882c
Sangeok <hamsoo159@naver.com>
2025-07-22 14:51:15 +0900

    refactor : Player

---
+++ Front/src/Features/Edit/ui/player/model/services/playerService.ts
@@ -0,0 +1,17 @@
+export const PlayerService = {
+  timeToFrame: (time: number, fps: number): number => {
+    return Math.floor(time * fps);
+  },
+
+  frameToTime: (frame: number, fps: number): number => {
+    return frame / fps;
+  },
+
+  roundTime: (time: number): number => {
+    return Math.round(time * 100) / 100;
+  },
+
+  getDurationInFrames: (duration: number, fps: number): number => {
+    return Math.floor(duration * fps) + 1; // ← 비즈니스 로직
+  },
+};
