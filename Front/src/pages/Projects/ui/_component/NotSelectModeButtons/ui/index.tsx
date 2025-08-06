327d38e3c92c87f7f3548975cf3b94e8e692a73e
Sangeok <hamsoo159@naver.com>
2025-08-06 15:09:27 +0900

    refactor : pages/Projects

---
+++ Front/src/pages/Projects/ui/_component/NotSelectModeButtons/ui/index.tsx
@@ -0,0 +1,31 @@
+import Button from "@/src/shared/ui/atoms/Button/ui/Button";
+import { MousePointer2, Plus } from "lucide-react";
+
+interface NotSelectModeButtonsProps {
+  setShowSelectMode: (show: boolean) => void;
+  setShowProjectManager: (show: boolean) => void;
+}
+
+export default function NotSelectModeButtons({
+  setShowSelectMode,
+  setShowProjectManager,
+}: NotSelectModeButtonsProps) {
+  return (
+    <div className="flex gap-2">
+      <Button
+        onClick={() => setShowSelectMode(true)}
+        variant="light"
+        className="flex items-center gap-2"
+      >
+        <MousePointer2 size={16} /> Select
+      </Button>
+      <Button
+        onClick={() => setShowProjectManager(true)}
+        variant="light"
+        className="flex items-center gap-2"
+      >
+        <Plus size={16} /> New Project
+      </Button>
+    </div>
+  );
+}
