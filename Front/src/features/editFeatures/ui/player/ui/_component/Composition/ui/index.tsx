cd732ad55b688513068cf91f0a5164fa4b06b34f
Sangeok <hamsoo159@naver.com>
2025-09-30 17:53:04 +0900

    refactor : delete inputProps

--- Front/src/features/editFeatures/ui/player/ui/_component/Composition/ui/index.tsx
+++ Front/src/features/editFeatures/ui/player/ui/_component/Composition/ui/index.tsx
@@ -2,17 +2,13 @@ import { TextElement } from "@/entities/media/types";
 import { SequenceItem } from "./_component/SequenceItem";
 import { useMediaStore } from "@/entities/media/useMediaStore";
 
-interface CompositionProps {
-  textElements: TextElement[];
-}
-
-export default function Composition({ textElements }: CompositionProps) {
+export default function Composition() {
   const { media } = useMediaStore();
   const fps = media.fps || 30;
 
   return (
     <>
-      {textElements.map((textElement) => {
+      {media.textElement.map((textElement) => {
         if (!textElement) return null;
         const trackItem = { ...textElement } as TextElement;
         return SequenceItem["text"](trackItem, { fps });
